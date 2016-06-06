edd88218307d3c4690ab1f1fc23dda94d04524dc
Marko Aalto <marko.aalto@reaktor.fi>
2016-06-06 18:30:19 +0300

    Very basic REPL

--- client/ponderon.js
+++ client/ponderon.js
@@ -1,4 +1,17 @@
-window.onload = function() {
-  console.log("duh")
-  d3.select("textarea#console").style('background-color', '#c0c0c0')
+window.onload = function () {
+  var input = d3.select('textarea#console')
+  var output = d3.select('div#output')
+  input.style('background-color', '#c0c0c0')
+  input.on('keyup', (e = d3.event) => {
+    if (e.keyCode == 13) {
+      process(input.property('value'), output)
+      input.property('value', '')
+    }
+  })
+}
+
+function process(text, output) {
+  var result =  eval(text)
+  output.append("di").text(text)
+  output.append("div").text('> ' + result)
 }
