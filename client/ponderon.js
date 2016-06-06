1ad8fdd3a4aa731713b9ce8751489eadef37f201
Marko Aalto <marko.aalto@reaktor.fi>
2016-06-06 23:16:24 +0300

    Grab focus

--- client/ponderon.js
+++ client/ponderon.js
@@ -2,6 +2,7 @@ window.onload = function () {
   var input = d3.select('textarea#console')
   var output = d3.select('div#output')
   input.style('background-color', '#c0c0c0')
+  input.node().focus()
   input.on('keyup', (e = d3.event) => {
     if (e.keyCode == 13) {
       process(input.property('value'), output)
