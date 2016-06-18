a371072cf8d70b568ad8fafa2cdaad9472a761db
Marko Aalto <marko.aalto@iki.fi>
2016-06-18 12:25:38 +0300

    Show errors

--- client/ponderon.js
+++ client/ponderon.js
@@ -1,20 +1,27 @@
 window.onload = function () {
   var input = d3.select('textarea#console')
   var output = d3.select('div#output')
+  var error = d3.select('div#error')
   input.style('background-color', '#c0c0c0')
   input.node().focus()
   input.on('keyup', (e = d3.event) => {
     if (e.keyCode == 13) {
       if (!e.shiftKey) {
-        process(input.property('value'), output)
-        input.property('value', '')
+        process(input, output, error)
       }
     }
   })
 }
 
-function process(text, output) {
-  var result =  eval(text)
-  output.append("div").attr('class', 'bubble').text(text)
-  output.append("div").attr('class', 'bubble output').text(result)
+function process(input, output, error) {
+  try {
+    var text = input.property('value')
+    var result = eval(text)
+    output.append("div").attr('class', 'bubble').text(text)
+    output.append("div").attr('class', 'bubble output').text(result)
+    error.style('visibility', 'hidden')
+    input.property('value', '')
+  } catch (e) {
+    error.style('visibility', 'visible').text(e)
+  }
 }
