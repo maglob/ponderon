f4595d98a080fba48e8b14d18bd919451c8bf7d9
Marko Aalto <marko.aalto@iki.fi>
2016-10-08 17:54:17 +0300

    Switch to notebook format

--- client/ponderon.js
+++ client/ponderon.js
@@ -1,15 +1,48 @@
-var rows = {}
+var rows = []
 var maxRowId = 1
 
-window.onload = function () {
-  var process = initialize(d3.select('textarea#console'),
-      d3.select('div#output'),
-      d3.select('div#error'),
-      d3.select('div#scrapbook'))
-  process('Math.atan(1) * 4')
+window.onload = () => initialize(d3.select('div#notebook'))
+
+function initialize(notebookContainer) {
+  createRow('Math.atan(1) * 4')
+  renderNotebook(notebookContainer, rows)
+  evalRow(notebookContainer.select("div.row div.input"))
 }
 
-function initialize(input, output, error, scrapbook) {
+function renderNotebook(container, rows) {
+  container.selectAll('div.row').data(rows)
+    .attr('contentEditable', 'true')
+    .text(d => d.text)
+    .enter()
+    .append('div')
+    .classed('row', true)
+    .attr('data-id', (d) => d.id)
+    .append('div')
+    .attr('contentEditable', 'true')
+    .classed('input', true)
+    .on('keyup', () => {
+      if (d3.event.ctrlKey && d3.event.keyCode == 13)
+        evalRow(d3.select(d3.event.target))
+    })
+    .text((d) => d.text)
+}
+
+function evalRow(sel) {
+  execute(sel.text(), (result) => {
+    result = typeof result == 'function' ? 'function: ' + result : result
+    d3.select(sel.node().parentNode).selectAll('div.output')
+      .data([result])
+      .text(result)
+      .enter()
+      .append('div')
+      .classed('output', true)
+      .text(result)
+  }, (error) => {
+    console.log("error", error)
+  })
+}
+
+function initialize2(input, output, error, scrapbook) {
   input.node().focus()
   input.on('keyup', (e = d3.event) => {
     if (e.ctrlKey) {
@@ -42,10 +75,28 @@ function initialize(input, output, error, scrapbook) {
               .classed('row', true)
               .attr('draggable', 'true')
               .attr('data-id', row.id)
+              .attr('contentEditable', 'true')
               .on('dragstart', (e = d3.event) => {
                 e.dataTransfer.setData('text/plain', row.id)
                 e.dataTransfer.dropEffect = 'copy'
               })
+            .on('keyup', (e = d3.event) => {
+              if (e.ctrlKey) {
+                switch (e.keyCode) {
+                  case 13:
+                    execute(d3.select(e.target).text(), (result) => {
+                      console.log(">", result)
+                      output.append('div')
+                        .attr('class', 'row output')
+                        .text(typeof result == 'function' ? 'function: ' + result : result)
+                        .node().scrollTop = output.node().getBoundingClientRect().height
+                    }, (error) => {
+                      console.log("error", error)
+                    })
+                    break;
+                }
+              }
+            })
           if (result != undefined) {
             output.append("div")
                 .attr('class', 'row output')
@@ -66,7 +117,7 @@ function createRow(str) {
     id: maxRowId++,
     text: str
   }
-  rows[row.id] = row
+  rows.push(row)
   return row
 }
 
